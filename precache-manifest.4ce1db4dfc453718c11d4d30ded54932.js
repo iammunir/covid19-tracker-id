7021fbbce515b7f09fd057e7cdb54ea0ec1c3884
mmunir <otlicnoe.pismo@gmail.com>
2020-04-26 00:05:48 +0800

    adding active case card and lastupdate

---
+++ precache-manifest.4ce1db4dfc453718c11d4d30ded54932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528413df75b3d71377aea1b6e51ba29a",
+    "url": "/iammunir/covid19-tracker-id/index.html"
+  },
+  {
+    "revision": "ed5f38c21a5904cafee7",
+    "url": "/iammunir/covid19-tracker-id/static/css/main.62afbe6d.chunk.css"
+  },
+  {
+    "revision": "1a8ae8219a195819b133",
+    "url": "/iammunir/covid19-tracker-id/static/js/2.cb4ff553.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/iammunir/covid19-tracker-id/static/js/2.cb4ff553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed5f38c21a5904cafee7",
+    "url": "/iammunir/covid19-tracker-id/static/js/main.587f4f8f.chunk.js"
+  },
+  {
+    "revision": "ebdb72aec3ed7941331b",
+    "url": "/iammunir/covid19-tracker-id/static/js/runtime-main.37785223.js"
+  }
+]);
